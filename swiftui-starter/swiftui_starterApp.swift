7794f935638643c1453285912cfc7db46c5928ca
Felipe Alves <profelipealves@outlook.com>
2023-07-05 09:46:54 -0300

    Initial Commit

---
+++ swiftui-starter/swiftui_starterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_starterApp.swift
+//  swiftui-starter
+//
+//  Created by Felipe Alves on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_starterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
